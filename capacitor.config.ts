3293fd8190d9cac178dbfef8b667bf6f0f0778af
Zsolt Boda <business@zsoltboda.com>
2024-01-17 20:03:25 +0100

    feat(android): :lipstick: add icons to application and change name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'better.onetab.extension',
-  appName: 'better-onetab-extension',
+  appName: 'Better OneTab',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
